d8368a2f0991b1ca3f90c56965ea8ac1ffdd8c81
mmimeault <michael.mimeault@gmail.com>
2016-09-23 12:14:35 -0400

    Initial project

---
+++ ParseLiveQuery/src/main/java/com/parse/WebSocketClient.java
@@ -0,0 +1,27 @@
+package com.parse;
+
+/* package */ interface WebSocketClient {
+
+    void open();
+
+    void close();
+
+    void send(String message);
+
+    State getState();
+
+    interface WebSocketClientCallback {
+        void onOpen();
+
+        void onMessage(String message);
+
+        void onClose();
+
+        void onError(Exception exception);
+
+        void stateChanged();
+    }
+
+    enum State {NONE, CONNECTING, CONNECTED, DISCONNECTING, DISCONNECTED}
+
+}
