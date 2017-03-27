ea2892fdd1ffe1a89fb252882b22f2b272581130
Roger Hu <roger.hu@gmail.com>
2017-03-27 09:09:24 -0700

    Bump to Android API 25 (#20)
    Bump Gradle versions
    
    Bump Roboelectric version
    
    Bump Travis
    
    Fix Travis YML
    
    Move to jcenter

--- ParseLiveQuery/src/test/java/com/parse/TestParseLiveQueryClient.java
+++ ParseLiveQuery/src/test/java/com/parse/TestParseLiveQueryClient.java
@@ -10,7 +10,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import java.net.URI;
@@ -36,7 +36,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class TestParseLiveQueryClient {
 
