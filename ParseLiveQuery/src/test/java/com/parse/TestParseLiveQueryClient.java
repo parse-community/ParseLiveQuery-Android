d8c515ac8ea7f2e298a408505ca629d8a08673b6
Herman Liang <hermanliang80@gmail.com>
2017-06-01 15:58:38 +0800

    Change package name (#64)
    * Change package name
    
    * update import in UT

--- ParseLiveQuery/src/test/java/com/parse/TestParseLiveQueryClient.java
+++ ParseLiveQuery/src/test/java/com/parse/TestParseLiveQueryClient.java
@@ -34,6 +34,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import com.parse.livequery.BuildConfig;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
